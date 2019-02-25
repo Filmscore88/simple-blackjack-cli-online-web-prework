cb61cafdd3f45e0ada516695fa3f2e7eddc7c871
Alberto Morales <cmtmorales24@gmail.com>
2019-02-25 15:34:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -18,7 +18,7 @@ def prompt_user
 end
 
 def get_user_input
-  gets 
+  gets.chomp 
 end
 
 def end_game(num)
@@ -35,7 +35,7 @@ end
 def hit?(num)
   prompt_user
   get_user_input 
-  if num=='s' then
+  if num=='s' 
     break
   
   elsif num=='h' then
