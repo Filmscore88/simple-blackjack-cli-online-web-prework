453616bee20e099735033ce6b9bf04fa5421ad7e
Alberto Morales <cmtmorales24@gmail.com>
2019-02-25 15:49:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -21,8 +21,8 @@ def get_user_input
   gets.chomp 
 end
 
-def end_game(num)
-  puts"Sorry, you hit #{num}. Thanks for playing!"
+def end_game(card_total)
+  puts"Sorry, you hit #{card_total}. Thanks for playing!"
   
 end
 
